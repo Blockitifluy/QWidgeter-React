c79adeeaa97ae4f943a7db7564eefc71b0dd41f1
Blockitifluy <136175716+Blockitifluy@users.noreply.github.com>
2023-10-02 17:33:10 +0100

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
+//#Basic React
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'; //Style
 
-function App() {
+//#Font Awesome (Icons)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+interface WidgetProps
+{
+  name : string
+  Position? : Vector
+}
+
+interface WidgetState
+{
+  name: string
+  Position: Vector
+}
+
+class Vector
+{
+  public x : number = 0;
+  public y : number = 0;
+
+  public toString() : string
+  {
+    return `${this.x}x${this.y}`;
+  }
+
+  constructor(x : number = 0, y : number = 0)
+  {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Widget extends React.Component<WidgetProps, WidgetState>
+{
+  render(): React.ReactNode
+  {
+    var positionStyle : React.CSSProperties = {
+      left: `${this.state.Position.x}px`,
+      top: `${this.state.Position.y}px`
+    };
+    
+    return (
+      <div className='widget' style={positionStyle}>
+        <h2 className='widget-meta-ing-marg'>{this.state.name}</h2>
+        <FontAwesomeIcon className='widget-delete widget-action' icon={faTrash}/>
+        <FontAwesomeIcon className='widget-delete widget-action' icon={faTrash}/>
+
+        <button className='widget-run'>Run</button>
+      </div>
+    )
+  }
+
+  constructor(props : WidgetProps)
+  {
+    super(props);
+
+    this.state = {
+      name: props.name,
+      Position: props.Position || new Vector(0, 0)
+    };    
+  }
+}
+
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="main-title">Qwidgeter</h1>
+
+      <Widget name='Hello World'/>
+    
     </div>
   );
 }
